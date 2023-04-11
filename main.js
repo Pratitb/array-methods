cf4084ec63a2000d9f75e757d9e246b07a69e55e
Pratit Bangdiwala <pratit.b@media.net>
2023-04-11 22:09:04 +0530

    format numbers, map method

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ let data =[]
 
 getRandomUser()
 getRandomUser()
-getRandomUser()
+
 
 async function getRandomUser(){
     const res = await fetch('https://randomuser.me/api')
@@ -22,18 +22,14 @@ async function getRandomUser(){
         name: `${user.name.first} ${user.name.last}`,
         money: Math.floor(Math.random() * 100000)
     }
-
     addData(newUser)
-
-    
 }
-
 // add new object to data array
 function addData(newUserObj){
     data.push(newUserObj)
     updateDOM()
 }
-
+// update DOM with elements
 function updateDOM(providedData = data){
     userNameWealth.innerHTML = ''
     providedData.forEach(item => {
@@ -41,14 +37,33 @@ function updateDOM(providedData = data){
         person.classList.add('person')
         person.innerHTML = 
         `<p class="username">${item.name}</p>
-        <p class="user_wealth">${item.money}</p>`
-        /* let oneUser = 
-        `<div class="person">
-            <ol type="1">
-            <li class="username">${item.name}</li>
-            </ol>
-            <p class="user_wealth">${item.money}</p>
-        </div>`; */
+        <p class="user_wealth">${formatNumber.format(item.money)}</p>`
         userNameWealth.appendChild(person)
     })
 }
+// format number as money
+let formatNumber = new Intl.NumberFormat('en-IN', {
+    style: 'currency',
+    currency: 'INR',
+    useGrouping: true, //default value
+    maximumSignificantDigits: 5,
+    //if this is 3, this means 108,289 will become 108,000
+});
+// doubleMoney
+function doubleMoney(){
+    data = data.map((user) => {
+        return {...user, money: user.money*2}
+    })
+    updateDOM()
+}
+
+/* new Intl.NumberFormat takes in 2 parameters. locales & options
+both are optional. Intl.NumberFormat can be called with or without new
+if no parameters are passed, it will format with commas only
+locales is geographical region. it does not add currency symbol
+options is the main parameter. it is a JS object which holds other params.
+refer above */
+
+// event listeners
+user.addEventListener('click', getRandomUser)
+double.addEventListener('click', doubleMoney)
