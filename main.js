2fe6d7ffdad6f5c8f999c1e022d3dbeb6ca30ac6
Pratit Bangdiwala <pratit.b@media.net>
2023-04-12 18:27:22 +0530

    add user, double amt, sort, total functionality

--- main.js
+++ main.js
@@ -4,18 +4,14 @@ const million = document.querySelector('#million')
 const rich = document.querySelector('#rich')
 const total = document.querySelector('#total')
 const userNameWealth = document.querySelector('.userNameWealth')
+const totalWealth = document.querySelector('.totalWealth')
 
 let data =[]
-
-getRandomUser()
-getRandomUser()
-
 
 async function getRandomUser(){
     const res = await fetch('https://randomuser.me/api')
     /* fetch happens behind the scenes, so basically we need to wait for it to finish */
     const data = await res.json()
-    console.log(data)
     const user = data.results[0]
 
     const newUser = {
@@ -46,7 +42,6 @@ let formatNumber = new Intl.NumberFormat('en-IN', {
     style: 'currency',
     currency: 'INR',
     useGrouping: true, //default value
-    maximumSignificantDigits: 5,
     //if this is 3, this means 108,289 will become 108,000
 });
 // doubleMoney
@@ -56,14 +51,47 @@ function doubleMoney(){
     })
     updateDOM()
 }
+// sort by rich
+function sortByRich(){
+    data.sort((a,b) => b.money - a.money)
+    updateDOM()
+}
+// filter millionaires
+function filterMillion(){
+    data = data.filter(millionaire => millionaire.money > 100000)
+    updateDOM()
+}
+// calculate wealth
+function calculateWealth(){
+    totalWealth.innerHTML = ''
+    const wealth = data.reduce((acc, user) => (acc += user.money),0)
+    console.log(wealth)
+    const totalEl = document.createElement('div')
+    totalEl.classList.add('person', 'total_amt_wrap')
+    totalEl.innerHTML = `<p class="total_text">Total Wealth:</p>
+                        <p class="total_amt">${formatNumber.format(wealth)}</p>`
+    totalWealth.appendChild(totalEl)
 
-/* new Intl.NumberFormat takes in 2 parameters. locales & options
-both are optional. Intl.NumberFormat can be called with or without new
-if no parameters are passed, it will format with commas only
-locales is geographical region. it does not add currency symbol
-options is the main parameter. it is a JS object which holds other params.
-refer above */
+}
+// call user function on DOM load
+function runFuncTimes(){
+    for(let i=0; i<3; i++){
+        getRandomUser()
+    }
+}
+
+/* 
+1. new Intl.NumberFormat takes in 2 parameters: locales & options. Both are optional.
+2. Intl.NumberFormat can be called with or without new.
+3. If no parameters are passed, it will format with commas only.
+4. locales is geographical region. it does not add currency symbol.
+5. options is the main parameter. it is a JS object which holds other params. Refer above
+*/
 
 // event listeners
 user.addEventListener('click', getRandomUser)
 double.addEventListener('click', doubleMoney)
+rich.addEventListener('click', sortByRich);
+million.addEventListener('click', filterMillion)
+total.addEventListener('click', calculateWealth)
+document.addEventListener('DOMContentLoaded', runFuncTimes)
